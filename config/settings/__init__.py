7b1a877fd924a4bca89a6edae88a9f8b18728392
shakhbozmn <shakhbozmn@gmail.com>
2026-02-26 18:23:00 +0500

    refactor: simplify settings initialization and improve documentation for clarity

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,21 +1,17 @@
 """
-Settings initialization - automatically loads development or production settings
-based on the DJANGO_SETTINGS_MODULE environment variable.
-
-For development: DJANGO_SETTINGS_MODULE=config.settings.development
-For production: DJANGO_SETTINGS_MODULE=config.settings.production
-
-If DJANGO_SETTINGS_MODULE is set to config.settings, defaults to development.
+Django settings package.
+By default, loads development settings unless DJANGO_SETTINGS_MODULE is set.
 """
-
 import os
 
-# Get the Django settings module from environment
-settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'config.settings')
+# Determine which settings to use
+settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
-# If it's just 'config.settings', default to development
-if settings_module == 'config.settings':
+# Import the appropriate settings
+if settings_module == 'config.settings.production':
+    from .production import *
+elif settings_module == 'config.settings.development':
     from .development import *
 else:
-    # Otherwise, the specific module will be loaded by Django
-    pass
+    # Fallback to development
+    from .development import *
