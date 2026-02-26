36ab41ab81a720c42edca1456f3e71d009a20b41
shakhbozmn <shakhbozmn@gmail.com>
2026-02-27 02:51:14 +0500

    fix: update database name and user in production settings for consistency

--- config/settings/production.py
+++ config/settings/production.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "4work_db"),
-        "USER": os.environ.get("DB_USER", "4work_user"),
+        "NAME": os.environ.get("DB_NAME", "fourwork_db"),
+        "USER": os.environ.get("DB_USER", "fourwork_user"),
         "PASSWORD": os.environ.get("DB_PASSWORD", ""),
         "HOST": os.environ.get("DB_HOST", "db"),
         "PORT": os.environ.get("DB_PORT", "5432"),
